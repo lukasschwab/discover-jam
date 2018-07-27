6f2981b8fb8fbb0331b42d4ba402818e2ab07ff2
Lukas Schwab <lukas.schwab@gmail.com>
2018-07-27 15:15:21 -0400

    Add real recommendations without filtering

--- go/cache.go
+++ go/cache.go
@@ -3,6 +3,7 @@ package main
 import (
   "log"
   "strings"
+  "strconv"
 
   "github.com/silentsokolov/go-vimeo/vimeo"
 )
@@ -18,10 +19,10 @@ var videoFansCache map[string][]string
 func (c Client) getUserLikes(userID string) ([]string, error) {
   videos, ok := userLikesCache[userID]
   if ok {
-    log.Print("Cache hit.")
+    log.Print("User cache hit.")
     return videos, nil
   } else {
-    log.Print("Cache miss.")
+    log.Print("User cache miss.")
     returned, _, err := c.Users.ListLikedVideo(userID)
     if err != nil {
       log.Print("Error from Vimeo API: ", err)
@@ -34,13 +35,45 @@ func (c Client) getUserLikes(userID string) ([]string, error) {
 }
 
 func APIFilterVideos(vids []*vimeo.Video) []string {
-  var out []string
   if vids == nil {
     return []string{}
   }
 
+  var out []string
   for _, vid := range vids {
     sliced := strings.Split(vid.URI, "/")
+    out = append(out, sliced[len(sliced) - 1])
+  }
+  return out
+}
+
+func (c Client) getVideoFans(videoID string) ([]string, error) {
+  fans, ok := videoFansCache[videoID]
+  if ok {
+    log.Print("Video cache hit.")
+    return fans, nil
+  } else {
+    log.Print("Video cache miss.")
+    idAsInt, _ := strconv.Atoi(videoID)
+    returned, _, err := c.Videos.LikeList(idAsInt)
+    if err != nil {
+      log.Print("Error from Vimeo API: ", err)
+      return nil, err
+    }
+    out := APIFilterUsers(returned)
+    videoFansCache[videoID] = out
+    return out, nil
+  }
+}
+
+func APIFilterUsers(fans []*vimeo.User) []string {
+  if fans == nil {
+    return []string{}
+  }
+
+  var out []string
+  for _, fan := range fans {
+    sliced := strings.Split(fan.URI, "/")
     out = append(out, sliced[len(sliced) - 1])
   }
   return out
--- go/recommendations.go
+++ go/recommendations.go
@@ -1,10 +1,13 @@
 package main
 
 import (
-	// "time"
+	"log"
+	"sync"
+	"sort"
 
 	"github.com/silentsokolov/go-vimeo/vimeo"
 	"golang.org/x/oauth2"
+	"github.com/ekzhu/counter"
 )
 
 type Client struct {
@@ -22,8 +25,62 @@ func NewClient() Client {
 }
 
 func (c Client) RecommendationsFor(userID string) ([]string, error) {
-	out, err := c.getUserLikes(userID)
-	return out, err
+	vids, err := c.getUserLikes(userID)
+	if err != nil {
+		return nil, err
+	}
+
+	var wg sync.WaitGroup
+	out := make(chan string, 1000000)
+	for x, vid := range vids {
+		wg.Add(1)
+		go c.compileRecs(vid, out, &wg)
+		if x > 5 { // FIXME
+			break
+		}
+	}
+	wg.Wait()
+	close(out)
+
+	ctr := counter.NewCounter()
+	for candidate := range out {
+		ctr.Update(candidate)
+	}
+	uqs, freqs := ctr.Freqs()
+	sort.Slice(uqs, func (i, j int) bool {
+		return freqs[i] > freqs[j]
+	})
+
+	var recs []string
+	for _, uq := range uqs[0:10] {
+		s, _ := uq.(string)
+		recs = append(recs, s)
+	}
+	// s, _ := uqs[0:10].([]string)
+
+	return recs, err
+}
+
+func (c Client) compileRecs(vid string, out chan string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	log.Print("Goroutine started.")
+	fans, err := c.getVideoFans(vid)
+	if err != nil {
+		return
+	}
+	for x, fan := range(fans) {
+		log.Print("Iterating over a video's fans.")
+		recCandidates, err := c.getUserLikes(fan)
+		if err != nil {
+			return
+		}
+		for _, r := range recCandidates {
+			out <- r
+		}
+		if x > 5 { // FIXME
+			break
+		}
+	}
 }
 
 var defaultValues = []int{96431363, 270062970, 277328815, 276246978, 276103410}
